fc62f74e7bc1c9de59b1742a33e3086a1303dec2
Hogward <jaegern23@gmail.com>
2023-10-24 12:14:41 -0500

    update code

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,14 @@ $(document).ready(function () {
   // Get references to the dropdown trigger and menu
   var dropdown = $(".dropdown");
   var searchButton = $("#searchButton");
-  var nextPageBtn = $("#nextButton");
-  var cardContainer = $("#card-container");
+//   var nextPageBtn = $("#nextButton");
 
   var typeTrigger = $("#typeDropTrigger");
-  var breedTrigger = $("#breedDropTrigger");
   var genderTrigger = $("#genderDropTrigger");
   var sizeTrigger = $("#sizeDropTrigger");
   var ageTrigger = $("#ageDropTrigger");
 
   var typeDropdown = $("#typeDrop");
-  var breedDropdown = $("#breedDrop");
   var genderDropdown = $("#genderDrop");
   var sizeDropdown = $("#sizeDrop");
   var ageDropdown = $("#ageDrop");
@@ -20,10 +17,6 @@ $(document).ready(function () {
   // Toggle the dropdown menu when the trigger button is clicked
   typeTrigger.click(function () {
     typeDropdown.toggleClass("is-active");
-  });
-
-  breedTrigger.click(function () {
-    breedDropdown.toggleClass("is-active");
   });
 
   genderTrigger.click(function () {
@@ -38,10 +31,12 @@ $(document).ready(function () {
     ageDropdown.toggleClass("is-active");
   });
 
-  nextPageBtn.click(function () {
-    removeDisplayPets();
-    //displayPets();
-  });
+ 
+
+//   nextPageBtn.click(function () {
+//     removeDisplayPets();
+//     //displayPets();
+//   });
 
   // Options clickable functions
 
@@ -53,12 +48,6 @@ $(document).ready(function () {
 
     // Remove the 'is-active' class from the dropdown to close it
     typeDropdown.removeClass("is-active");
-  });
-
-  breedDropdown.find(".dropdown-item").click(function () {
-    var selectedItemText = $(this).text();
-    $("#selectedBreedText").text(selectedItemText);
-    breedDropdown.removeClass("is-active");
   });
 
   genderDropdown.find(".dropdown-item").click(function () {
@@ -86,99 +75,71 @@ $(document).ready(function () {
     }
   });
 
-  searchButton.click(function () {
-    var data = [
-      {
-        title: "Item 1",
-        subtitle: "Subtitle 1",
-        content: "This is the content for Item 1.",
+  searchButton.click(getPets);
+  
+
+  var cardContainer = $("#card-container");
+  var petAPI = "UmVWwKPzfv9io8hZanQhhe2T5CC3Ns1Bdf2F6JEevSuotzH35V";
+  var petURL = "https://api.petfinder.com/v2/animals/";
+  // Here are the docs : https://www.petfinder.com/developers/v2/docs/
+
+  function getPets() {
+    removeDisplayPets();
+    
+    fetch(petURL, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
       },
-      {
-        title: "Item 2",
-        subtitle: "Subtitle 2",
-        content: "This is the content for Item 2.",
-      },
-      {
-        title: "Item 3",
-        subtitle: "Subtitle 3",
-        content: "This is the content for Item 3.",
-      },
-      {
-        title: "Item 4",
-        subtitle: "Subtitle 4",
-        content: "This is the content for Item 4.",
-      },
-      {
-        title: "Item 5",
-        subtitle: "Subtitle 5",
-        content: "This is the content for Item 5.",
-      },
-    ];
-    //show filter items
-    // call api and pass data to this function
-    displayPets(data);
-  });
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Request failed with status: " + response.status);
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        displayPets(data);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
 
   function displayPets(data) {
-    var card = $("<div>").addClass("card");
-    var cardContent = $("<div>").addClass("card-content");
-    var type = $("<p>")
-      .addClass("type")
-      .text("Type: " + data[0].title);
-    console.log(data[0].title);
-    var location = $("<p>")
-      .addClass("location")
-      .text("Location: " + data.location);
-    var breed = $("<p>")
-      .addClass("breed")
-      .text("Breed: " + data.breed);
-    var content = $("<p>").text("Description: " + data.description);
+    var animals = data.animals;
+    for (var i = 0; i < animals.length; i++) {
+      var animal = animals[i];
 
-    var cardImg = $("<div>").addClass("card-image");
-    var figure = $("<figure>").addClass("image is-4by3");
+      var card = $("<div>").addClass("card");
+      var cardContent = $("<div>").addClass("card-content");
+      var name = $("<p>").addClass("name").text("Name: " + animal.name);
+      var breed = $("<p>").addClass("breed").text("Breed: " + animal.breeds.primary);
+      var content = $("<p>").text("Description: " + animal.description);
 
-    var img = $("<img>")
-      //.attr("src", data[0].photos.url)
-      .attr("alt", "Pet Image");
+      var cardImg = $("<div>").addClass("card-image");
+      var figure = $("<figure>").addClass("image is-4by3");
 
-    var cardFooter = $("<footer>").addClass("card-footer");
-    cardContent.append(type, location, breed, content, cardImg);
-    card.append(cardContent, cardFooter);
-    figure.append(img);
-    cardImg.append(figure);
-    cardContainer.append(card);
+      if (animal.photos.length > 0) {
+        var img = $('<img>').addClass('photo').attr('src',animal.photos[0].medium).attr("alt", "Pet Image");
+    }
+      
+      cardContent.append(name, breed, content, cardImg);
+      card.append(cardContent,);
+      figure.append(img);
+      cardImg.append(figure);
+      cardContainer.append(card);
+    }
   }
 
   function removeDisplayPets() {
     cardContainer.empty();
+    
   }
 });
 
-var petAPI = "UmVWwKPzfv9io8hZanQhhe2T5CC3Ns1Bdf2F6JEevSuotzH35V";
-var petURL = "https://api.petfinder.com/v2/animals/";
-// Here are the docs : https://www.petfinder.com/developers/v2/docs/
-
-function getPets() {
-  fetch(petURL, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Request failed with status: " + response.status);
-      }
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
-}
 
 //picture slideshow functionality:
 const slides = document.querySelectorAll(".slide");
@@ -207,3 +168,55 @@ setInterval(nextSlide, 3000);
 
 // Show the initial slide
 showSlide(currentSlide);
+
+//This is the map API
+
+function initMap() {
+  let mapOptions = {
+    center: new google.maps.LatLng("41.881832", "-87.623177"),
+    zoom: 12,
+  };
+
+  let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+initMap();
+//here is the access token storage
+// Assuming you have obtained the access token and have it in a variable
+var accessToken = localStorage.getItem("access_token");
+
+// Store the access token in local storage
+localStorage.setItem("access_token", accessToken);
+
+//This grabs the access token from the local storage.
+
+document.addEventListener("DOMContentLoaded", function () {
+  const clientId = "UmVWwKPzfv9io8hZanQhhe2T5CC3Ns1Bdf2F6JEevSuotzH35V";
+  const clientSecret = "BBqThKtQ3eJuDRQDR1EOCrnGZ2CurI1Vy6PIAG35";
+  const tokenUrl = "https://api.petfinder.com/v2/oauth2/token";
+
+  const data = {
+    grant_type: "client_credentials",
+    client_id: clientId,
+    client_secret: clientSecret,
+  };
+
+  fetch(tokenUrl, {
+    method: "POST",
+    body: new URLSearchParams(data),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const accessToken = data.access_token;
+      // You have obtained the access token, you can store it in local storage or use it as needed.
+      console.log("Access Token: " + accessToken);
+      localStorage.setItem("access_token", accessToken);
+    })
+    .catch((error) => {
+      console.error("Error fetching access token: ", error);
+    });
+});
+console.log(accessToken);
+// getPets();
